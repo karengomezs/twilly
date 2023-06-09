7e54357ae94fc844ba39120954536b7596acb36d
karengomezs <karengomezsoto1@gmail.com>
2023-06-09 16:39:24 -0500

    posts readys

--- src/app/components/sign-in-form.tsx
+++ src/app/components/sign-in-form.tsx
@@ -11,7 +11,7 @@ export default function SignInForm() {
   const [password, setPassword] = useState<string>();
 
   return (
-    <div className="flex flex-col items-center  text-lime-500 font-semibold">
+    <div className="flex flex-col items-center text-lime-500 font-semibold">
       <h2 className="text-2xl mb-5">Log In</h2>
       <Form.Root className="flex flex-col gap-4">
         <Form.Field className="mx-auto" name="email">
@@ -58,7 +58,7 @@ export default function SignInForm() {
               }
             }}
             type="submit"
-            className="w-60 p-2 mt-3 mx-auto rounded-md bg-lime-500 text-center text-gray-950"
+            className="w-60 p-2 mt-3 mx-auto rounded-md bg-lime-500 text-center font-bold text-gray-950"
           >
             Sign In
           </button>
--- src/app/content/page.tsx
+++ src/app/content/page.tsx
@@ -1,3 +1,96 @@
+"use client";
+
+import * as Form from "@radix-ui/react-form";
+import * as Avatar from "@radix-ui/react-avatar";
+import { useState } from "react";
+
 export default function Content() {
-  return <h1 className="text-white">hola content</h1>;
+  const [postContent, setPostContent] = useState<string>("");
+  const [postsArray, setPostsArray] = useState<string[]>([]);
+
+  console.log(postsArray);
+
+  let posts = postsArray.map((post, i) => {
+    return (
+      <div
+        key={post}
+        className="bg-gray-900 py-5 px-5 w-[70%] mx-auto rounded-md  text-lime-500 font-semibold"
+      >
+        <div className="flex gap-8 items-center">
+          <Avatar.Root className="">
+            <Avatar.Image
+              className="w-12 h-12 rounded-full bg-gray-700 flex items-center justify-center object-cover"
+              src="https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80"
+              alt="Colm Tuite"
+            />
+            <Avatar.Fallback className="" delayMs={600}>
+              CT
+            </Avatar.Fallback>
+          </Avatar.Root>
+          <p className="text-xl">nombre usuario</p>
+        </div>
+        <div className="ml-20">
+          <p className=" text-gray-300 pb-3">{post}</p>
+          <div className="flex gap-3 [&>button]:p-2 [&>button]:bg-gray-950 [&>button]:rounded-md">
+            <button>💚</button>
+            <button>🔗</button>
+          </div>
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <div className="bg-gray-900 py-5 px-5 mt-20 w-[70%] mx-auto rounded-md flex gap-5 text-lime-500 font-semibold">
+        <Avatar.Root className="">
+          <Avatar.Image
+            className="w-12 h-12 rounded-full bg-gray-700 flex items-center justify-center object-cover"
+            src="https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80"
+            alt="Colm Tuite"
+          />
+          <Avatar.Fallback className="" delayMs={600}>
+            CT
+          </Avatar.Fallback>
+        </Avatar.Root>
+
+        <Form.Root className="flex items-center gap-5 w-full">
+          <Form.Field className="w-full" name="email">
+            <Form.Control className="w-full" asChild>
+              <input
+                onChange={(e) => {
+                  setPostContent(e.target.value);
+                }}
+                value={postContent}
+                className="flex-1 w-full p-2 rounded-md"
+                placeholder="What're you thinking? 🧐"
+                type="text"
+                required
+              />
+            </Form.Control>
+          </Form.Field>
+
+          <Form.Submit asChild>
+            <button
+              onClick={() => {
+                setPostsArray([postContent, ...postsArray]);
+                setPostContent("");
+              }}
+              type="submit"
+              className=" p-2 rounded-md bg-lime-500 text-center text-gray-950"
+            >
+              POST
+            </button>
+          </Form.Submit>
+        </Form.Root>
+      </div>
+
+      {/* ------------ posts div */}
+      <div className="mt-10 flex flex-col gap-5">
+        {/* primer post */}
+        {posts}
+        {/* termina primer post */}
+      </div>
+    </div>
+  );
 }
